b299cdb108e0756967f2a9a3dcf12ab2d2dc5bcf
Shane Jeffery <nobull.wow@gmail.com>
2020-02-06 00:35:33 -0800

    Updates to stack view button alignment.

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -54,6 +54,28 @@ class PlaySoundsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        snailButton.contentMode = .center
+        snailButton.imageView?.contentMode = .scaleAspectFit
+        
+        chipmunkButton.contentMode = .center
+        chipmunkButton.imageView?.contentMode = .scaleAspectFit
+        
+        rabbitButton.contentMode = .center
+        rabbitButton.imageView?.contentMode = .scaleAspectFit
+        
+        vaderButton.contentMode = .center
+        vaderButton.imageView?.contentMode = .scaleAspectFit
+        
+        echoButton.contentMode = .center
+        echoButton.imageView?.contentMode = .scaleAspectFit
+        
+        reverbButton.contentMode = .center
+        reverbButton.imageView?.contentMode = .scaleAspectFit
+        
+        stopButton.contentMode = .center
+        stopButton.imageView?.contentMode = .scaleAspectFit
+        
         setupAudio()
     }
     
